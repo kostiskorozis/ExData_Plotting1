01cd09b1dda32e616260ba69225b9bea7a7a5bfd
Kostis Korozis <kostis.korozis@gmail.com>
2020-12-28 04:10:46 +0200

    assignment submission

--- plot1.R
+++ plot1.R
@@ -4,7 +4,7 @@ power <- subset(power, Date == "1/2/2007" | Date == "2/2/2007")
 
 #plot1
 png(filename = "plot1.png", width = 480, height = 480)
-hist(power$Global_active_power, col = "orange",
+hist(power$Global_active_power, col = "red",
       xlab = "Global Active Power (kilowatts)", 
       ylab = "Frequency", main = "Global Active Power")
 
